657b340a9b201266895e9555828eae325f8bcf72
doyler <dev@doyler.net>
2014-01-30 11:54:12 -0500

    Update getHash, connections

--- tyrant.py
+++ tyrant.py
@@ -1,4 +1,3 @@
-# -*- coding: cp1252 -*-
 import httplib
 import hashlib
 import time
@@ -30,12 +29,15 @@ class tyrant_test(object):
         self.time_hash = "fgjk380vf34078oi37890ioj43"
         self.myTime = str(int(time.time())/900)
 
+        self.conn = httplib.HTTPConnection('kg.tyrantonline.com')
+        self.conn.set_debuglevel(1)
+
         #ccache = hashlib.md5()
         #ccache.update(self.myTime)
         #ccache.update(self.user_id)
         #ccache = ccache.hexdigest()
 
-    def getHash(self, message):
+    def getHash(self, time, message):
         reqHash = hashlib.md5()
         reqHash.update(message)
         reqHash.update(self.myTime)
@@ -45,63 +47,43 @@ class tyrant_test(object):
 
     def init(self):
         message = "init"
-        self.myTime = "0"
         
-    
         path = "/api.php?user_id="+self.user_id+"&message="+message
-        data = "?&flashcode="+self.flashcode+"&time="+self.myTime+"&version=&hash="+reqHash+"&ccache=&client_code="+self.client_code+"&game_auth_token="+self.game_auth_token+"&rc=2"
-    
-        conn = httplib.HTTPConnection('kg.tyrantonline.com')
-        conn.set_debuglevel(1)
-        conn.request("POST", path, data, self.headers)
+        data = "?&flashcode="+self.flashcode+"&time="+self.myTime+"&version=&hash="+self.getHash("0", message)+"&ccache=&client_code="+self.client_code+"&game_auth_token="+self.game_auth_token+"&rc=2"
+        
+        self.conn.request("POST", path, data, self.headers)
 
-        decompressed_data = gzip.GzipFile('', 'rb', 9, StringIO.StringIO(conn.getresponse().read())).read()
+        decompressed_data = gzip.GzipFile('', 'rb', 9, StringIO.StringIO(self.conn.getresponse().read())).read()
         json_data = json.loads(decompressed_data)
-        conn.close()
+        self.conn.close()
         
         self.client_code = json_data["client_code"]
 
     def getFactionNews(self):
         message = "getFactionNews"
-        self.myTime = str(int(time.time())/900)
-        reqHash = hashlib.md5()
-        reqHash.update(message)
-        reqHash.update(self.myTime)
-        reqHash.update(self.time_hash)
-        reqHash = reqHash.hexdigest()
-    
+            
         path = "/api.php?user_id="+self.user_id+"&message="+message
-        data = "&flashcode="+self.flashcode+"&time="+self.myTime+"&version="+self.version+"&hash="+reqHash+"&ccache=&client_code="+str(self.client_code)+"&game_auth_token="+self.game_auth_token+"&rc=2"
+        data = "&flashcode="+self.flashcode+"&time="+self.myTime+"&version="+self.version+"&hash="+self.getHash(self.myTime, message)+"&ccache=&client_code="+str(self.client_code)+"&game_auth_token="+self.game_auth_token+"&rc=2"
 
-        conn = httplib.HTTPConnection('kg.tyrantonline.com')
-        conn.set_debuglevel(1)
-        conn.request("POST", path, data, self.headers)
+        self.conn.request("POST", path, data, self.headers)
 
-        decompressed_data = gzip.GzipFile('', 'rb', 9, StringIO.StringIO(conn.getresponse().read())).read()
+        decompressed_data = gzip.GzipFile('', 'rb', 9, StringIO.StringIO(self.conn.getresponse().read())).read()
         json_data = json.loads(decompressed_data)
         
-        conn.close()
+        self.conn.close()
         return json_data
 
     def getFactionMembers(self):
         message = "getFactionMembers"
-        self.myTime = str(int(time.time())/900)
-        reqHash = hashlib.md5()
-        reqHash.update(message)
-        reqHash.update(self.myTime)
-        reqHash.update(self.time_hash)
-        reqHash = reqHash.hexdigest()
     
         path = "/api.php?user_id="+self.user_id+"&message="+message
-        data = "&flashcode="+self.flashcode+"&time="+self.myTime+"&version="+self.version+"&hash="+reqHash+"&ccache=&client_code="+str(self.client_code)+"&game_auth_token="+self.game_auth_token+"&rc=2"
+        data = "&flashcode="+self.flashcode+"&time="+self.myTime+"&version="+self.version+"&hash="+self.getHash(self.myTime, message)+"&ccache=&client_code="+str(self.client_code)+"&game_auth_token="+self.game_auth_token+"&rc=2"
 
-        conn = httplib.HTTPConnection('kg.tyrantonline.com')
-        conn.set_debuglevel(1)
-        conn.request("POST", path, data, self.headers)
+        self.conn.request("POST", path, data, self.headers)
 
-        decompressed_data = gzip.GzipFile('', 'rb', 9, StringIO.StringIO(conn.getresponse().read())).read()
+        decompressed_data = gzip.GzipFile('', 'rb', 9, StringIO.StringIO(self.conn.getresponse().read())).read()
         
-        conn.close()
+        self.conn.close()
         return decompressed_data    
 
 myTyrant = tyrant_test()
